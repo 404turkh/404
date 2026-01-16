9cbeb45b9263cfb5c4db6b6c1d22607e9ea1ab37
404turkh <404turkh@gmail.com>
2026-01-16 23:43:43 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ list1: [
 
 {logo:"snd.png",name:"Dns Profile",desc:"",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/byharambro.mobileconfig"},
 {logo:"snd.png",name:"Dns Profile",desc:"Config",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/bypassrevokedns.mobileconfig"},
-{logo:"ngisk.png",name:"KSign",desc:"Qingdao Rural Commercial Bank Co., Ltd",url:"https://api.khoindvn.eu.org/KXcveB"},
+{logo:"ngisk.png",name:"KSign",desc:"Qingdao Rural Commercial Bank Co., Ltd",url:"https://api.khoindvn.eu.org/XSCvQT"},
 {logo:"ngisk.png",name:"KSign",desc:"Commission on Elections",url:"https://api.khoindvn.io.vn/g65UJy"},
 {logo:"ngisk.png",name:"KSign Com V1",desc:"Commission on Elections",url:"https://api.khoindvn.io.vn/ZsgCVX"},
 {logo:"ngisk.png",name:"KSign Com V2",desc:"Commission on Elections",url:"https://api.khoindvn.io.vn/VNNlkL"},
