4ef32496e2d149541af95fe0229195ef1c6500c8
404turkh <404turkh@gmail.com>
2026-02-09 17:33:20 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ list1: [
 ],
 
 list2: [
-{logo:"tw.png",name:"esing",desc:"Certificate",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/ESignCert.zip"},
+{logo:"tw.png",name:"esing",desc:"Certificate",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/ESignCert.zip "},
 
 {logo:"gnise.png",name:"ESign",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/IDcW4P"},
 {logo:"gnise.png",name:"ESign PowerChina",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/NvH0ZF"},
