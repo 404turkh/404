0592720da177ceedf165ede1cf0bb5814ae9a963
404turkh <404turkh@gmail.com>
2026-02-12 22:50:44 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -429,6 +429,7 @@ list1: [
 {logo:"snd.png",name:"Dns Profile",desc:"",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/byharambro.mobileconfig"},
 {logo:"snd.png",name:"Dns Profile",desc:"Config",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/bypassrevokedns.mobileconfig"},
 
+{logo:"ngisk.png",name:"KSign✅",desc:"Forevermark Marketing (Shanghai) Limited",url:"https://api.khoindvn.io.vn/YRBC4f"},
 {logo:"ngisk.png",name:"KSign",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/03zH0o"},
 {logo:"ngisk.png",name:"KSign PowerChina",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/03zH0o"},
 {logo:"ngisk.png",name:"KSign PowerChina 1",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/QdVVh3"},
@@ -469,6 +470,7 @@ list1: [
 list2: [
 {logo:"tw.png",name:"esing",desc:"Certificate",url:"https://github.com/404turkh/404/releases/download/%EA%B3%A1/ESignCert.zip "},
 
+{logo:"gnise.png",name:"ESign✅",desc:"Forevermark Marketing (Shanghai) Limited",url:"https://api.khoindvn.io.vn/aAqGZk"},
 {logo:"gnise.png",name:"ESign",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/IDcW4P"},
 {logo:"gnise.png",name:"ESign PowerChina",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/NvH0ZF"},
 {logo:"gnise.png",name:"ESign PowerChina 1",desc:"PowerChina International Group Limited",url:"https://api.khoindvn.io.vn/GKJa03"},
